fd878fa4e099e3d4919ff52a55a280bcc3309162
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-05-23 11:40:29 +0200

    second commit

--- date-object.js
+++ date-object.js
@@ -26,6 +26,7 @@ console.log("6", day);
 const nowToo = new Date().getDate();
 console.log("7", nowToo);
 
+//Write an if/else statment, logging the current season from the getMonth Method
 const showSeason = new Date().getMonth() + 1;
 
 if (showSeason === 12 || showSeason === 1 || showSeason === 2) {
@@ -38,19 +39,28 @@ if (showSeason === 12 || showSeason === 1 || showSeason === 2) {
     console.log("8: It is Autumn in Berlin");
 }
 
-
+//8: or
 const winter = showSeason === 12 || showSeason === 1 || showSeason === 2;
 const spring = showSeason === 3 || showSeason === 4 || showSeason === 5;
 const summer = showSeason === 6 || showSeason === 7 || showSeason === 8;
-
 if (winter) {
-    console.log("9: It is winter in Berlin");
+    console.log("or: It is winter in Berlin");
 } else if (spring) {
-    console.log("9: It is Spring in Berlin");
+    console.log("or: It is Spring in Berlin");
 } else if (summer) {
-    console.log("9: It is Summer in Berlin");
+    console.log("or: It is Summer in Berlin");
 } else {
-    console.log("9: It is Autumn in Berlin");
+    console.log("or: It is Autumn in Berlin");
+}
+//8: 0r
+if (showSeason <= 2 || showSeason === 12) {
+    console.log("or: winter");
+} else if (showSeason <= 5) {
+    console.log("or: Spring");
+} else if (showSeason <= 8) {
+    console.log("or; Summer");
+} else if (showSeason <= 11) {
+    console.log("or: Autumn");
 }
 
 
